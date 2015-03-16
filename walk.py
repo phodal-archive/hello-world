45f6ae7d7b0e6a40a6295bd636565f9a834eecaf
Fengda HUANG <h@phodal.com>
2015-03-16 08:50:53 +0800

    Add all results

--- walk.py
+++ walk.py
@@ -14,7 +14,12 @@ def generate_json(dir_name, files_name):
 
     with open('data/' + dir_name + '.json', 'w') as outfile:
         json.dump(data, outfile)
+    return data
 
+results = []
 for directory in ['lv1', 'lv2', 'lv3', 'lv4']:
     for root, dirs, files in os.walk(directory):
-        generate_json(directory, files)
+        results.append(generate_json(directory, files))
+
+with open('data/results.json', 'w') as outfile:
+        json.dump(results, outfile)
